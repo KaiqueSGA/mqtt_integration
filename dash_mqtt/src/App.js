da3f3966fb1ec71e1fcaaa1ebd65cbf4230dc67c
KaiqueYudji <yudjikaique@gmail.com>
2022-11-22 14:05:18 -0300

    almost_ready

--- dash_mqtt/src/App.js
+++ dash_mqtt/src/App.js
@@ -4,18 +4,16 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const {Account, Device, Services} = require('@tago-io/sdk'); 
 
+    
+    const notify = (message) => toast.success(message);
+    const error = (message) => toast.error(message) 
+    
+
+
 let render = 0;
 function App() {
    const account = new Account({token:"a394b4c9-7ea7-4346-bbe1-ddb35f9bc948"});
    const [arrayOfdevices,setArray] = useState([]);
-
-
-
-
-    const notify = () => toast.success("Configuration performed!");
-    const toastAlert = () => toast.error("Configuration already exists!");
-    const AlertError = () => toast.error("There are more than 1 comands in the queue, it's not possible to add more commands.");
-    const error = () => toast.error("There is a command in the file, it's not possible to add more commands before of execute that is in the file.") 
     
 
     async function listdevices(){
@@ -27,13 +25,11 @@ function App() {
        } 
 
 
-
         useEffect( () => {
             listdevices()
         } )
         
    
-
         window.TagoIO.onStart((widget) => {
             window.widget = widget;
         })
@@ -41,41 +37,52 @@ function App() {
  
        
 
-        function insertParam(){
-          let selectDevice = document.getElementById("devices");
-          let device_token = selectDevice.options[selectDevice.selectedIndex].value;
-    
-          
+        async function insertParam(){
+            let selectDevice = document.getElementById("devices");
+            let device_token = selectDevice.options[selectDevice.selectedIndex].value.split(',')[0];
+            let device_name = selectDevice.options[selectDevice.selectedIndex].value.split(",")[1];          
+            
+
+          let parameters_values = new Array();
           const get_configuration_parameters = async () => {
-            const myDevice = new Device({ token: device_token });
+             try{
+                  const myDevice = new Device({ token: device_token });
 
-            const result = await myDevice.getParameters();
-            console.log(result)
-          }
+                  const result = await myDevice.getParameters();
+                  result.forEach((parameter) => parameters_values.push({
+                       key:parameter.key,
+                       value:parameter.value
+                    })
+                  );//in this code line, i'm catching all the values of device's configuration parameter and i'm storing in an array. 
 
-          get_configuration_parameters()
+              }catch(err){
+                error(`${err}, please verify the device's tag. Something can be wrong`)
+              }
+
+            };await get_configuration_parameters();
+            
+
+            
+            
+
+            const send_mqtt_to_device = async () => {
+    
+                    let formated_string = `ID=1234567890ABCDEF,MODE=${ (parameters_values.find((parameter) => {return parameter.key === 'MODE'})).value  },TIMER_GPRS=${ (parameters_values.find((parameter) => {return parameter.key === 'TIMER_GPRS'})).value  },TIMER_GPS=${ (parameters_values.find((parameter) => {return parameter.key === 'TIMER_GPS'})).value  },TIMER_SMS=${ (parameters_values.find((parameter) => {return parameter.key === 'TIMER_SMS'})).value  },CMD=${ (parameters_values.find((parameter) => {return parameter.key === 'CMD'})).value  }`;
+                    const options = { retain: false, qos: 2 };
+                     
+                    
+                    const MQTT = new Services({ token: "61f848bb-e3d0-4a13-836a-ef90d84834c3"}).MQTT;
+                     MQTT.publish({
+                        bucket: (arrayOfdevices.find((dev) => dev.name === device_name)).id , //device's bucket ? FROM CONTEXT OR SCOPE
+                        message: JSON.stringify(formated_string), //the MQTT message
+                        topic: (arrayOfdevices.find((dev) => dev.name === device_name)).tags.find(tag => { return tag.key === "ESN"}).value, //ESN or IMEI
+                        options,
+                    }).then((resp) => notify(resp))   
+                      .catch((err) => error(err))
+
+            };send_mqtt_to_device();
+
       }
-
-
-  /* const myDataObject = "ID=1234567890ABCDEF,MODE=NULL,TIMER_GPRS=1,TIMER_GPS=1,TIMER_SMS=1,CMD=1";
-
-  const options = {
-    retain: false,
-    qos: 2,
-  };
-
-
-  const MQTT = new Services({ token: "61f848bb-e3d0-4a13-836a-ef90d84834c3"}).MQTT;
-
-
-  MQTT.publish({
-    bucket: '63653142ae603e0011629b86', //device's bucket ? FROM CONTEXT OR SCOPE
-    message: JSON.stringify(myDataObject), //the MQTT message
-    topic: "869951032050266", //ESN or IMEI
-    options,
-  }).then((resp) => console.log(resp)) */
-
-
 
 
         return(
@@ -92,51 +99,32 @@ function App() {
                 
                             <div className=" label_dev">
                                 <img className ="drop" alt='' src="https://img.icons8.com/external-zen-filled-royyan-wijaya/2x/external-list-dropdown-business-zen-filled-royyan-wijaya.png" />
-                                <div className ="title_label">Selecione os Dispositivos</div> 
+                                <div className ="title_label">Select the device to configure it</div> 
                             </div>
                 
                 
-                             <div className="input">
+                              <div className="input">
                                     <select id="devices" className ="devices">
                                     {arrayOfdevices.map((item) =>{
-                                      let device_token = item.tags.find(dev => {return dev.key === "device_token"});
+                                        try{
+                                            let device_token = item.tags.find(dev => {return dev.key === "device_token"});
                                             return(
-                                              <option value={device_token.value}>{item.name}</option>
+                                                <option value={`${device_token.value},${item.name}`}> {item.name} </option>
                                             )
+                                        }catch(err){
+                                           error(err)
+                                        }
+                                      
                                     })}
                                     </select>
-                                </div> 
+                                </div>  
                                 
-                
-                
-                
-                            <div className ="secondInput">
-                
-                                <div className =" label_dev">
-                                    <img alt='' className ="drop" src="https://img.icons8.com/windows/2x/expand-arrow.png"/>
-                                    <div className ="title_label">CMD</div> 
-                                </div>
-                
-                                
-                                <select id="configs" className ="inputCmd">
-                                    <option value="Carga último perfil da plataforma">Carga último perfil da plataforma</option>
-                                    <option value="Solicitar Identificação ID">Solicitar Identificação ID</option>
-                                    <option value="Reiniciar Dispositivo">Reiniciar dispositivo</option>
-                                    <option value="Ligar Modo Emergência">Ligar Modo Emergência</option>
-                                    <option value="Ligar Modo Normal">Ligar Modo Normal</option>
-                                    <option value="Solicita Posição LBS / GPS">Solicita Posição LBS / GPS</option>
-                                    <option value="Status da Bateria">Status da Bateria</option>
-                                    <option value="Alterar Frequência RF433">Alterar Frequência RF433</option>
-                                    <option value="Alterar Melodia RF433">Alterar Melodia RF433</option>
-                                </select>
-                
-                            </div>
                 
                             
                 
                             <div className="rodape">
                                  <button className="button" onClick={insertParam} >
-                                    Enviar CMD
+                                    Sent MQTT Message
                                 </button> 
                             </div>
                 
