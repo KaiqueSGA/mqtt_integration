149df28cf2a1dbbb478f164ecda6213fef26a031
KaiqueYudji <yudjikaique@gmail.com>
2022-11-23 12:19:41 -0300

    ready_with_token_on_tag

--- dash_mqtt/src/App.js
+++ dash_mqtt/src/App.js
@@ -14,6 +14,7 @@ let render = 0;
 function App() {
    const account = new Account({token:"a394b4c9-7ea7-4346-bbe1-ddb35f9bc948"});
    const [arrayOfdevices,setArray] = useState([]);
+   let parameters_values = new Array();
     
 
     async function listdevices(){
@@ -29,11 +30,53 @@ function App() {
             listdevices()
         } )
         
+
    
         window.TagoIO.onStart((widget) => {
             window.widget = widget;
         })
         window.TagoIO.ready();
+
+
+
+
+        const get_configuration_parameters = async (device_token) => {
+            try{
+                 const myDevice = new Device({ token: device_token });
+
+                 const result = await myDevice.getParameters();
+                 result.forEach((parameter) => parameters_values.push({
+                      key:parameter.key,
+                      value:parameter.value
+                   })
+                 );//in this code line, i'm catching all the values of device's configuration parameter and i'm storing in an array. 
+
+             }catch(err){
+               error(`${err}, please verify the device's tag. Something can be wrong`)
+             }
+
+           };
+
+
+
+
+           const send_mqtt_to_device = async (device_name) => {
+
+                
+                let formated_string = `ID=${(Math.floor(Math.random() * 1000000000)).toString(16)},MODE=${ (parameters_values.find((parameter) => {return parameter.key === 'MODE'})).value  },TIMER_GPRS=${ (parameters_values.find((parameter) => {return parameter.key === 'TIMER_GPRS'})).value  },TIMER_GPS=${ (parameters_values.find((parameter) => {return parameter.key === 'TIMER_GPS'})).value  },TIMER_SMS=${ (parameters_values.find((parameter) => {return parameter.key === 'TIMER_SMS'})).value  },CMD=${ (parameters_values.find((parameter) => {return parameter.key === 'CMD'})).value  }`;
+                const options = { retain: false, qos: 2 };
+                
+                
+                 const MQTT = new Services({ token: "61f848bb-e3d0-4a13-836a-ef90d84834c3"}).MQTT;//this toke it's a token of an external analysis. It will be static
+                MQTT.publish({
+                    bucket: (arrayOfdevices.find((dev) => dev.name === device_name)).id , //device's bucket ? FROM CONTEXT OR SCOPE
+                    message: JSON.stringify(formated_string), //the MQTT message
+                    topic: (arrayOfdevices.find((dev) => dev.name === device_name)).tags.find(tag => { return tag.key === "ESN"}).value, //ESN or IMEI
+                    options,
+                }).then((resp) => notify(resp))   
+                .catch((err) => error(err)) 
+
+           }
  
        
 
@@ -41,46 +84,9 @@ function App() {
             let selectDevice = document.getElementById("devices");
             let device_token = selectDevice.options[selectDevice.selectedIndex].value.split(',')[0];
             let device_name = selectDevice.options[selectDevice.selectedIndex].value.split(",")[1];          
-            
-
-          let parameters_values = new Array();
-          const get_configuration_parameters = async () => {
-             try{
-                  const myDevice = new Device({ token: device_token });
-
-                  const result = await myDevice.getParameters();
-                  result.forEach((parameter) => parameters_values.push({
-                       key:parameter.key,
-                       value:parameter.value
-                    })
-                  );//in this code line, i'm catching all the values of device's configuration parameter and i'm storing in an array. 
-
-              }catch(err){
-                error(`${err}, please verify the device's tag. Something can be wrong`)
-              }
-
-            };await get_configuration_parameters();
-            
-
-            
-            
-
-            const send_mqtt_to_device = async () => {
-    
-                    let formated_string = `ID=1234567890ABCDEF,MODE=${ (parameters_values.find((parameter) => {return parameter.key === 'MODE'})).value  },TIMER_GPRS=${ (parameters_values.find((parameter) => {return parameter.key === 'TIMER_GPRS'})).value  },TIMER_GPS=${ (parameters_values.find((parameter) => {return parameter.key === 'TIMER_GPS'})).value  },TIMER_SMS=${ (parameters_values.find((parameter) => {return parameter.key === 'TIMER_SMS'})).value  },CMD=${ (parameters_values.find((parameter) => {return parameter.key === 'CMD'})).value  }`;
-                    const options = { retain: false, qos: 2 };
-                     
-                    
-                    const MQTT = new Services({ token: "61f848bb-e3d0-4a13-836a-ef90d84834c3"}).MQTT;
-                     MQTT.publish({
-                        bucket: (arrayOfdevices.find((dev) => dev.name === device_name)).id , //device's bucket ? FROM CONTEXT OR SCOPE
-                        message: JSON.stringify(formated_string), //the MQTT message
-                        topic: (arrayOfdevices.find((dev) => dev.name === device_name)).tags.find(tag => { return tag.key === "ESN"}).value, //ESN or IMEI
-                        options,
-                    }).then((resp) => notify(resp))   
-                      .catch((err) => error(err))
-
-            };send_mqtt_to_device();
+         
+            await get_configuration_parameters(device_token);
+            await send_mqtt_to_device(device_name);
 
       }
 
